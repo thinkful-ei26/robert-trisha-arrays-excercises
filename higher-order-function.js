ce86e431e6d86d4bf63c636c79f784199379ab2d
RP Boyle <rpboyle11@yahoo.com>
2018-10-30 15:17:34 -0400

    turtle movements filter

--- higher-order-function.js
+++ higher-order-function.js
@@ -17,29 +17,82 @@
 // repeat(hello(), 5);
 // repeat(goodbye(), 5);
 
-function filter(arr, fn) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++){
-    let currentElement = arr[i];
-    if (fn(currentElement) === true){
-      newArray.push(currentElement);
-    }
-  }
-  //return newArray
-  return newArray;
-}
+// function filter(arr, fn) {
+//   let newArray = [];
+//   for (let i = 0; i < arr.length; i++){
+//     let currentElement = arr[i];
+//     if (fn(currentElement) === true){
+//       newArray.push(currentElement);
+//     }
+//   }
+//   //return newArray
+//   return newArray;
+// }
 
-// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
-// First we setup an array of strings we plan to filter:
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// // First we setup an array of strings we plan to filter:
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-// We use your `filter` function here, capturing a new array into `filteredNames`
-// comprised of names that only begin with 'R'
-const filteredNames = filter(myNames, function(name) {
-  // This is known as a "predicate function" - it's a function that 
-  // only returns a boolean
-  return name[0] === 'R';
-});
+// // We use your `filter` function here, capturing a new array into `filteredNames`
+// // comprised of names that only begin with 'R'
+// const filteredNames = filter(myNames, function(name) {
+//   // This is known as a "predicate function" - it's a function that 
+//   // only returns a boolean
+//   return name[0] === 'R';
+// });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// // <---- DO NOT EDIT BETWEEN THESE LINES
+
+
+// function hazardWarningCreator(typeOfWarning) {
+//   let warningCounter = 0;
+//   return function(location) {
+//     warningCounter++;
+//     console.log(`Danger there is a ${typeOfWarning} hazard at ${location}!`)
+//     console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
+//   }
+// }
+
+// const rocksWarning = hazardWarningCreator('Rocks on the Road')
+// const floodWarning = hazardWarningCreator('Theres a flood coming!');
+// const tornadoWarning = hazardWarningCreator('There are flying houses everywhere!')
+
+// rocksWarning('Main St and Pacific Ave');
+// // => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// // => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+// floodWarning('Centinela Ave and Olympic Blvd');
+// // => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// // => The Rocks on the Road hazard has triggered 2 time(s) today!
+
+// // Invoke the other hazard creators you instantiated, too
+
+let turtleMovements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+// turtleMovements.filter(function(){
+//     for (let i = 0; i <= turtleMovements.length -1; i++){
+//       if (turtleMovements[i][0] < 0 || turtleMovements[i][1] < 0){
+//         turtleMovements.splice(i);
+//       }
+//     }
+//     console.log(turtleMovements);
+//   });
+
+// turtleMovements.filter(function(movement) {
+//       movement[0] <=0 && movement[1] <= 0;
+//       console.log(turtleMovements);
+//     })
+
+turtleMovements = turtleMovements.filter(movement => movement[0] >= 0 && movement[1] >= 0);
+console.log(turtleMovements);
+
+  // forEach, filter, map
+// let movements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+// movements = movements.filter(movement => movement[0] >= 0 && movement[1] >= 0);
+
+// distances = movements.map(movement => movement[0] + movement[1]);
+
+// distances.forEach(distance => {
+//     console.log(distance);
+// });
